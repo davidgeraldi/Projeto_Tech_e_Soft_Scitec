d434fe95af5104d1a393213b4cc0ae2f4f3eeac7
Francisco Arthur <francisarthfernandes@gmail.com>
2026-06-30 15:30:32 -0300

    feat: forms add book

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -19,6 +20,36 @@ app.get("/biblioteca", (req, res) => {
 
   const books = JSON.parse(bd || "[]");
   res.render("library", { books: books });
+});
+
+app.post("/salvar-leitura", (req, res) => {
+  // Pega os dados do formulário
+  const { titulo, autor, paginas } = req.body;
+
+  const numPaginas = parseInt(paginas);
+  if (numPaginas < 1) {
+    console.log("Tentativa de salvar páginas inválidas.");
+    return res.redirect("/");
+  }
+
+  let books = [];
+  if (fs.existsSync("bd.json")) {
+    const bd = fs.readFileSync("bd.json", "utf-8");
+    books = JSON.parse(bd || "[]");
+  }
+
+  const novoLivro = {
+    id: Date.now(),
+    titulo: titulo,
+    autor: autor,
+    paginas: numPaginas,
+  };
+
+  books.push(novoLivro);
+
+  fs.writeFileSync("bd.json", JSON.stringify(books, null, 2), "utf-8");
+
+  res.redirect("/biblioteca");
 });
 
 app.get("/contato", (req, res) => {
